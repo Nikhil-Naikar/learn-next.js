0aa3061fa458a2294dcc654ac32339a7a96e1063
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-08-02 11:29:48 -0400

    learned about Next.js important features like rounting(file structure), server component data fetching, server actions(use client & use server, when to run code on server or client)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
-import Image from "next/image";
+import { TodoItem } from "@/components/TodoItem"
+import { prisma } from "@/db"
+import Link from "next/link"
 
-export default function Home() {
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+
+  await prisma.todo.update({ where: { id }, data: { complete } })
+}
+
+export default async function Home() {
+  const todos = await getTodos()
+
   return (
-    <h1>Hello World</h1>
-  );
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map(todo => (
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+        ))}
+      </ul>
+    </>
+  )
 }
